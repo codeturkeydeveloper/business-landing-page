9e7c7eaf2e2bda91c165d39b629cc946109fa01f
Turkey Coder <244347825+codeturkeydeveloper@users.noreply.github.com>
2025-11-17 02:51:27 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,9 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vuetify from 'vite-plugin-vuetify'
 
 export default defineConfig({
     base: '/business-landing-page/',
-    plugins: [vue(),
-    vuetify(),],
+    plugins: [vue()],
     build: {
         outDir: 'docs',
         rollupOptions: {
