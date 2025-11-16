83bc1a296d37ba500010bbeccb1525e6849e3800
Turkey Coder <244347825+codeturkeydeveloper@users.noreply.github.com>
2025-11-17 03:14:04 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,26 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vuetify from 'vite-plugin-vuetify'
+import { viteSingleFile } from 'vite-plugin-singlefile'
 
 export default defineConfig({
     base: '/business-landing-page/',
-    plugins: [vue(), vuetify()],
-    build: {
-        outDir: 'docs'
+    plugins: [vue(), viteSingleFile()],
+    css: {
+        preprocessorOptions: {
+            scss: {}
+        }
+    },
+   build: {
+        outDir: 'docs',
+        rollupOptions: {
+            output: {
+                entryFileNames: 'index.js',
+                assetFileNames: 'index.[ext]',
+                manualChunks: undefined,
+                inlineDynamicImports: true,
+            },
+        },
+        assetsInlineLimit: Infinity,
     },
     server: {
         port: 5173,
